32cebe1ae7e1daa42342ee945c0346203e1ff3ec
Anderson Barros Coimbra <andersonmegax@hotmail.com>
2022-05-08 05:00:58 -0300

    Implementação de sqlite

---
+++ lib/helpers/DatabaseHelper.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:flutter_sqlite/models/Grocery.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "groceries.db");
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE GROCERIES(
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+''');
+  }
+
+  Future<List<Grocery>> getGroceries() async {
+    Database db = await instance.database;
+    var groceries = await db.query("groceries", orderBy: "name");
+    List<Grocery> groceryList = groceries.isNotEmpty
+        ? groceries.map((c) => Grocery.fromMap(c)).toList()
+        : [];
+    return groceryList;
+  }
+
+  Future<int> add(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.insert("groceries", grocery.toMap());
+  }
+
+  Future<int> remove(int id) async {
+    Database db = await instance.database;
+    return await db.delete("groceries", where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<int> update(Grocery grocery) async {
+    Database db = await instance.database;
+    return db.update("groceries", grocery.toMap(),
+        where: "id = ?", whereArgs: [grocery.id]);
+  }
+}
---
+++ lib/models/Grocery.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+class Grocery {
+  final int? id;
+  final String name;
+
+  Grocery({this.id, required this.name});
+
+  factory Grocery.fromMap(Map<String, dynamic> json) =>
+      new Grocery(id: json["id"], name: json["name"]);
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+    };
+  }
+}
